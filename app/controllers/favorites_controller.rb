d34452fea1e6f6aa21d59b80b42bd92df16a3471
tomohiro.yasukura <a0025575@yahoo.co.jp>
2020-06-12 15:10:15 +0000

    favorite

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,17 @@
+class FavoritesController < ApplicationController
+  def create
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.favorite(micropost)
+    flash[:success] = 'お気に入りに登録しました。'
+    #元のページへ戻る
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.unfavorite(micropost)
+    flash[:success] = 'お気に入り登録を解除しました。'
+    #元のページへ戻る
+    redirect_back(fallback_location: root_path)
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :favorites]
   
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
@@ -37,6 +37,12 @@ class UsersController < ApplicationController
   def followers
     @user = User.find(params[:id])
     @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+  
+  def likes
+    @user = User.find(params[:id])
+    @favorite_posts = @user.favorite_posts.page(params[:page])
     counts(@user)
   end
 
